85f71f080babb9b2c1a0a208da465c29ebf7a121
umedha33 <umedharaj@gmail.com>
2024-02-23 14:31:26 +0530

    footer last

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,12 +12,12 @@ const Footer = () => {
             <img id='cake-a-boo' src={cakeabooText} alt="cake-a-boo" />
             <div className="foot-elements">
                 <div className="sec-one">
-                    <h1>About Us:</h1>
+                    <h1>About Us</h1>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit magni sunt officiis pariatur perferendis deleniti esse itaque beatae dolores iusto dolorem, saepe expedita commodi voluptas non! Obcaecati sunt officiis pariatur perferendis deleniti esse itaque beatae dolores iusto iste fuga quis!</p>
                     <h2>From our oven to your heart, with love.</h2>
                 </div>
                 <div className="sec-two">
-                    <h1>Quick Links:</h1>
+                    <h1>Quick Links</h1>
                     <ul>
                         <li>Cakes</li>
                         <li>Generate Designs</li>
@@ -29,10 +29,16 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div className="sec-three">
-                    <h2>set three</h2>
+                    <h1>Contact</h1>
+                    <ul>
+                        <li><i class="fa-regular fa-envelope"></i>  cakeaboocolombo@gmail.com</li>
+                        <li><i class="fa-brands fa-instagram"></i> @cake_a_boo_colombo</li>
+                        <li><i class="fa-solid fa-map-pin"></i> Cake A Boo - Colombo 06</li>
+                        {/* <li><i class="fa-solid fa-phone"></i> 081 234 5678</li> */}
+                    </ul>
                 </div>
                 <div className="sec-four">
-                    <h2>set four</h2>
+                    <h1>Promo Alerts</h1>
                 </div>
             </div>
             <div className="bottm-segmnt">
