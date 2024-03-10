e5fe24089f4a76ed31d0ca2f33e2fa6899ea82be
umedha33 <umedharaj@gmail.com>
2024-03-10 12:17:18 +0530

    product gallery upd

--- src/components/ProductsRender/ProductsRender.jsx
+++ src/components/ProductsRender/ProductsRender.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './ProductsRender.css'
+import dummyProducts from '../Assets/dummy-products.js'
 
 const ProductsRender = () => {
     return (
         <div className='product-render-container'>
-            <h1>Products Renderer</h1>
+            {dummyProducts.map(product => (
+                <div key={product.id} className="ind-product">
+                    <img src={product.image} alt={product.title} />
+                    <button>ADD TO CART</button>
+                    <div className="ind-product-details">
+                        <h3>{product.title}</h3>
+                        {/* <p id='indcategory'>Category: {product.category}</p> */}
+                        <p id='indprice'>{(product.price).toFixed(2)} LKR</p>
+                    </div>
+
+                </div>
+            ))}
         </div>
     )
 }
