e4cf8810084f31723b0c3620a619912ee398f9c5
umedha33 <umedharaj@gmail.com>
2024-04-15 22:54:05 +0530

    chat ui done

--- src/components/UserDashboard/UserDashboard.jsx
+++ src/components/UserDashboard/UserDashboard.jsx
@@ -6,10 +6,13 @@ const UserDashboard = () => {
     return (
         <div className='userdashboard-container'>
             <div className="userdash-header">
-                <h1 id='myorder-lbl'>My Orders ›</h1>
+                <div className="myorder-lbl">
+                    <h1>My Orders</h1>
+                    <h1>CHAT</h1>
+                </div>
                 <button id='logoutBtn'>LOGOUT</button>
             </div>
-            <div className="user-orders-container">
+            {/* <div className="user-orders-container">
                 <table className='orders-body'>
                     <tbody>
                         {dummyOrders.map((order, index) => (
@@ -27,6 +30,49 @@ const UserDashboard = () => {
                         ))}
                     </tbody>
                 </table>
+            </div> */}
+            <div className="user-chat-container">
+                <div className="chat-instructions">
+                    <h1>Chat instructions and policies</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sapiente cupiditate aliquid veniam vero perspiciatis, libero possimus laudantium impedit explicabo dignissimos, minus obcaecati earum beatae, in quia error cumque! Quas.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sapiente cupiditate aliquid veniam vero perspiciatis, libero possimus laudantium impedit explicabo dignissimos, minus obcaecati earum beatae, in quia error cumque! Quas.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sapiente cupiditate aliquid veniam vero perspiciatis, libero possimus laudantium impedit explicabo dignissimos, minus obcaecati earum beatae, in quia error cumque! Quas.</p>
+                </div>
+                <div className="cht-body">
+                    <h1>Chat with Cake A Boo</h1>
+                    <div className="msg-body">
+                        <div className="chat-threads">
+                            <div className="sender-side">
+                                <p id='sender-msg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis officia ad dicta error corrupti.</p>
+                            </div>
+                            <div className="user-side">
+                                <p id='user-msg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis officia ad dicta error corrupti.</p>
+                                <p id='user-msg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis officia ad dicta error corrupti. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis officia ad dicta error corrupti.</p>
+                            </div>
+                            <div className="sender-side">
+                                <p id='sender-msg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis officia ad dicta adipisicing elit. Perferendis officia ad dicta error</p>
+                            </div>
+                            <div className="user-side">
+                                <p id='user-msg'>Perferendis officia ad dicta error corrupti.</p>
+                            </div>
+                            <div className="sender-side">
+                                <p id='sender-msg'>consectetur adipisicing elit. Perferendis officia ad dicta error corrupti dolor sit amet, consectetur adipisicing elit. Perferendis officia ad dicta error</p>
+                            </div>
+                        </div>
+                        <div className="text-sender">
+                            <input type="text" name="message-txt" id="message-txt" placeholder='Enter message' />
+                            <input
+                                type="file"
+                                id="file-input"
+                                style={{ display: 'none' }}
+                            />
+                            <label htmlFor="file-input" className="file-label">
+                                <i className="fa-solid fa-file"></i>
+                            </label>
+                            <i class="fa-solid fa-paper-plane"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
