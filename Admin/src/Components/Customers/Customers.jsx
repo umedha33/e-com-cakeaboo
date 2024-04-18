2b8aa679ffe88753ef10757d58232d4b204dffd9
Ethugal Rajaratne <121920950+umedha33@users.noreply.github.com>
2024-04-19 00:41:51 +0530

    chat page update

--- Admin/src/Components/Customers/Customers.jsx
+++ Admin/src/Components/Customers/Customers.jsx
@@ -11,53 +11,59 @@ const Customers = () => {
 
     return (
         <div className='customers-container'>
-            <div className="chat-lst">
-                <div className="chat-hdr">
-                    <h1>Chat List</h1>
-                </div>
-                <div className="chat-lst-body">
-                    {dummyChats.map((chat, index) => (
-                        <div
-                            key={index}
-                            className={`cht-lstcard ${index === currentChatIndex ? 'active-chat' : ''}`}
-                            onClick={() => selectChat(index)}
-                        >
-                            <div className='card-dtls'>
-                                <h2>{chat.customerName}</h2>
-                                <p>{chat.conversation[chat.conversation.length - 1].message}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+            <div className="chats-nav">
+                <h1>CHAT LIST</h1>
+                <h1>MESSAGE</h1>
             </div>
-
-            <div className="cht-body">
-                <h1>Message</h1>
-                <div className="msg-body">
-                    {currentChatIndex === null && <h2 id='slct-chat-lbl'>Select a chat</h2>}
-                    <div className="chat-threads">
-                        {currentChatIndex !== null && dummyChats[currentChatIndex].conversation.map((msg, index) => (
-                            <div key={index} className={msg.sender !== "Me" ? "sender-side" : "user-side"}>
-                                <p id={msg.sender !== "Me" ? "user-msg" : "sender-msg"}>{msg.message}</p>
+            <div className="cht-portal">
+                <div className="chat-lst">
+                    {/* <div className="chat-hdr">
+                        <h1>Chat List</h1>
+                    </div> */}
+                    <div className="chat-lst-body">
+                        {dummyChats.map((chat, index) => (
+                            <div
+                                key={index}
+                                className={`cht-lstcard ${index === currentChatIndex ? 'active-chat' : ''}`}
+                                onClick={() => selectChat(index)}
+                            >
+                                <div className='card-dtls'>
+                                    <h2>{chat.customerName}</h2>
+                                    <p>{chat.conversation[chat.conversation.length - 1].message}</p>
+                                </div>
                             </div>
                         ))}
                     </div>
+                </div>
+                <div className="cht-body">
+                    {/* <h1>Message</h1> */}
+                    <div className="msg-body">
+                        {currentChatIndex === null && <h2 id='slct-chat-lbl'>Select a chat</h2>}
+                        <div className="chat-threads">
+                            {currentChatIndex !== null && dummyChats[currentChatIndex].conversation.map((msg, index) => (
+                                <div key={index} className={msg.sender !== "Me" ? "sender-side" : "user-side"}>
+                                    <p id={msg.sender !== "Me" ? "user-msg" : "sender-msg"}>{msg.message}</p>
+                                </div>
+                            ))}
+                        </div>
 
-                    <div className="text-sender">
-                        <input type="text" name="message-txt" id="message-txt" placeholder='Enter message' />
-                        <input
-                            type="file"
-                            id="file-input"
-                            style={{ display: 'none' }}
-                        />
-                        <label htmlFor="file-input" className="file-label">
-                            <i className="fa-solid fa-file"></i>
-                        </label>
-                        <i class="fa-solid fa-paper-plane"></i>
+                        <div className="text-sender">
+                            <input type="text" name="message-txt" id="message-txt" placeholder='Enter message' />
+                            <input
+                                type="file"
+                                id="file-input"
+                                style={{ display: 'none' }}
+                            />
+                            <label htmlFor="file-input" className="file-label">
+                                <i className="fa-solid fa-file"></i>
+                            </label>
+                            <i class="fa-solid fa-paper-plane"></i>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
